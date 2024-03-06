36aa934bda5415632089bfc0699c7d2f8f2f0bc3
Edems <57250017+Edems-DEV@users.noreply.github.com>
2024-03-06 21:21:15 +0100

    <3

--- script.js
+++ script.js
@@ -3,13 +3,8 @@ let tasks = []
 LoadData()
 // ---SETUP---
 function LoadData(){
-    const cProject = document.querySelector('#currentProject')
-    projects.forEach(p => {
-        const x = document.createElement('option')
-        x.setAttribute('value', p.id)
-        x.innerText = p.name
-        cProject.appendChild(x)
-    })
+    //loadTasks()
+    loadProjects()
 
     const task = {
         name: "Návrch šablony",
@@ -25,35 +20,65 @@ function LoadData(){
     const project2 = { id: "2", name: "Projekt 2"}
 
     projects.push(project)
-    renderProjects()
+    projects.push(project2)
     tasks.push(task)
-    renderTasks()
 
+    const cProject = document.querySelector('#currentProject')
+    projects.forEach(p => {
+        const x = document.createElement('option')
+        x.setAttribute('value', p.id)
+        x.innerText = p.name
+        cProject.appendChild(x)
+    })
+
+    renderProjects()
+    renderTasks()
 }
 //-----------------------HEADER--------------------------------------
 const cForm = document.querySelector('#currentForm')
 const cBtn = document.querySelector('#btnStartStop')
 const cInput = document.querySelector('#currentText')
 const cProject = document.querySelector('#currentProject')
+let started = false;
+let currentTask;
+let cInterval;
+const cHead = document.querySelector('#clockH')
 cForm.addEventListener('submit', (e) => {
     e.preventDefault();
+    if(!started){
+        started = true;
+        cBtn.classList.remove('circle')
+        currentTask = {
+            name: cInput.value,
+            projectId: cProject.value,
+            start: new Date(),
+            end: null
+        };
 
-    const task = {
-        name: cInput.value,
-        projectId: cProject.value,
-        start: new Date(),
-        end: null
-    };
-    tasks.push(task)
-    renderTasks();
-    saveTasks();
+        cInterval = setInterval(() => {
+            currentTask.end =  new Date(new Date - currentTask.start)
+            cHead.innerText = currentTask.end.getUTCHours().toString().padStart(2, '0')+':'+currentTask.end.getMinutes().toString().padStart(2, '0')+':'+currentTask.end.getSeconds().toString().padStart(2, '0')
+        }, 100)
+    }else{ //STOP
+        started = false;
+        clearInterval(cInterval)
+        cHead.innerText = "00:00:00"
+        currentTask.end = new Date()
+
+        cBtn.classList.add('circle')
+        tasks.push(currentTask)
+        renderTasks();
+        saveTasks();
+
+
+    }
 })
 
 //-----------------------------TASK--------------------------------------
 function renderTasks() {
     document.querySelectorAll('#taskTable *').forEach(e => e.remove());
 
-    tasks.forEach((c) => {
+    tasks.forEach((c, index) => {
         const area = document.querySelector("#taskTable")
         const tr = document.createElement('tr');
         const th1 = document.createElement('th');
@@ -72,15 +97,20 @@ function renderTasks() {
         th2.innerText = projects.find(p => p.id === c.projectId).name
         sp1.innerText = c.start.toLocaleDateString()
         sp2.innerText = c.start.getHours().toString().padStart(2, '0')+':'+c.start.getMinutes().toString().padStart(2, '0')
-        sp3.innerText = c.end.toLocaleDateString()
-        sp4.innerText = c.end.getHours().toString().padStart(2, '0')+':'+c.end.getMinutes().toString().padStart(2, '0')
-        const diff = new Date(c.end - c.start)
-        th5.innerText = diff.getHours().toString().padStart(2, '0')+':'+diff.getMinutes().toString().padStart(2, '0')+':'+diff.getSeconds().toString().padStart(2, '0')
+        if (c.end != null){
+            sp3.innerText = c.end.toLocaleDateString()
+            sp4.innerText = c.end.getHours().toString().padStart(2, '0')+':'+c.end.getMinutes().toString().padStart(2, '0')
+            const diff = new Date(c.end - c.start)
+            console.log(c.start)
+            console.log(c.end)
+            console.log(diff)
+            th5.innerText = diff.getUTCHours().toString().padStart(2, '0')+':'+diff.getUTCMinutes().toString().padStart(2, '0')+':'+diff.getUTCSeconds().toString().padStart(2, '0')
+        }
         btnDel.innerText = "smazat"
         btnDel.addEventListener('click', (e) => {
-            tasks.splice(c.index,1)
+            tasks.splice(index,1)
             renderTasks()
-            //delete in cash
+            saveTasks() //delete in cash
         })
 
         tr.appendChild(th1)
@@ -98,8 +128,17 @@ function renderTasks() {
     });
 }
 function saveTasks() {
-    const carsJson = JSON.stringify(cars);
-    localStorage.setItem('cars', carsJson);
+    const Json = JSON.stringify(tasks);
+    localStorage.setItem('tasks', Json);
+}
+function loadTasks() {
+    const Json = localStorage.getItem('tasks');
+    if (!Json) {
+        tasks = [];
+        return;
+    }
+    tasks = JSON.parse(Json);
+    renderTasks();
 }
 
 //--------------PROJECTS-------------------------------------
@@ -113,10 +152,27 @@ formProjectEdit.addEventListener('submit', (e) => {
     projects[editSender].name = inputProjectEdit.value
     //renderProjects() //JS => není třeba refresh
 })
+const dialogProjectCreate = document.querySelector('#dialogNewTaskProject');
+const formProjectCreate = dialogProjectCreate.querySelector('form')
+const inputProjectCreate = dialogProjectCreate.querySelector('input')
+formProjectCreate.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let id = 1
+    if (projects[projects.length - 1].id){ id = parseInt(projects[projects.length - 1].id) + 1}
+    const project = {
+        id: id,
+        name: inputProjectCreate.value
+    }
+    projects.push(project)
+    renderProjects()
+    saveProjects()
+    dialogProjectCreate.Close()
+    console.log(project)
+})
 function renderProjects() {
     document.querySelectorAll('#projectTable *').forEach(e => e.remove());
 
-    projects.forEach((c) => {
+    projects.forEach((c, index) => { //index is cool
         const area = document.querySelector("#projectTable")
         const tr = document.createElement('tr');
         const th1 = document.createElement('th');
@@ -134,13 +190,13 @@ function renderProjects() {
         let t = new Date(total)
 
         th1.innerText = c.name
-        th2.innerText = t.getHours().toString().padStart(2, '0')+':'+t.getMinutes().toString().padStart(2, '0')+':'+t.getSeconds().toString().padStart(2, '0')
+        th2.innerText = t.getUTCHours().toString().padStart(2, '0')+':'+t.getUTCMinutes().toString().padStart(2, '0')+':'+t.getUTCSeconds().toString().padStart(2, '0')
         btnDel.innerText = "smazat"
         btnEdit.innerText = "upravit"
         btnDel.addEventListener('click', (e) => {
-            projects.splice(c.index,1)
+            projects.splice(index,1)
             renderProjects()
-            //delete in cash
+            saveProjects()
         })
         btnEdit.addEventListener('click', (e) => {
             editSender = c.id;
@@ -155,7 +211,19 @@ function renderProjects() {
         area.appendChild(tr)
     });
 }
-
+function saveProjects() {
+    const projectsJson = JSON.stringify(projects);
+    localStorage.setItem('projects', projectsJson);
+}
+function loadProjects() {
+    const Json = localStorage.getItem('projects');
+    if (!Json) {
+        projects = [];
+        return;
+    }
+    projects = JSON.parse(Json);
+    renderProjects();
+}
 
 // --- OPEN DIALOG ---
 const btnTask = document.querySelector('#btnTask');
@@ -168,7 +236,7 @@ const dialogProject = document.querySelector('#dialogNewTaskProject');
 btnProject.addEventListener('click', (e) => {
     dialogProject.showModal()
 })
-dialogProject.addEventListener('submit', (e) => {
-    e.preventDefault()
-    projects[editSender].name = inputProjectEdit.value
-})
+// dialogProject.addEventListener('submit', (e) => {
+//     e.preventDefault()
+//     projects[editSender].name = inputProjectEdit.value
+// })
